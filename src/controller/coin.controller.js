a0a92a77b83ac1f6c6fd7ba2d94b54c7ada03086
Yash Shakya <yashshakya40@gmail.com>
2025-01-11 03:53:04 +0530

    feat(coin.controller): implemented /deviation route (Task 3)

--- src/controller/coin.controller.js
+++ src/controller/coin.controller.js
@@ -1,7 +1,7 @@
 import Coin from "../model/coin.model.js";
 
 
-// Task 2
+// Task 2: function to get coin data
 export async function getCoinData(req, res) {
     const {coin} = req.query;
 
@@ -25,3 +25,32 @@ export async function getCoinData(req, res) {
 }
 
 
+// Task 3: function to get deviation
+export async function getDeviation(req,res){
+    const {coin} = req.query;
+
+    if(!coin){
+        return res.status(400).json({message:"coin is required"});
+    }
+
+    try {
+        const coinData=await Coin.findOne({name:coin});
+        if(!coinData){
+            return res.status(404).json({message:"coin not found"});
+        }
+        const n = coinData.price.length;
+        if(n<100){
+            const mean = coinData.price.reduce((a,b)=>a+b)/n;
+            const deviation = coinData.price.map((price)=>(price-mean)**2).reduce((a,b)=>a+b)/n;
+            return res.status(200).json({deviation});
+        }
+        else{
+            const mean = coinData.price.slice(n-100,n).reduce((a,b)=>a+b)/100;
+            const deviation = coinData.price.slice(n-100,n).map((price)=>(price-mean)**2).reduce((a,b)=>a+b)/100;
+            return res.status(200).json({deviation});
+        }
+        
+    } catch (error) {
+        return res.status(500).json({message:"Internal server error"});
+    }
+}
--- src/routes/coin.route.js
+++ src/routes/coin.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getCoinData } from "../controller/coin.controller.js";
+import { getCoinData,getDeviation } from "../controller/coin.controller.js";
 
 const router = express.Router();
 
 router.get("/stats", getCoinData);
+router.get("/deviation", getDeviation);
+
 
 export default router;
